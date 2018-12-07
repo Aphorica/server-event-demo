760e17e5a9fddc8df89f866148f3cb64515fdfd6
Rick Berger <rickb@aphorica.com>
2018-12-07 13:04:31 -0700

    Factored out ServerEvent

---
+++ app/ServerEvent.js
@@ -0,0 +1,286 @@
+let express = require('express');
+let serverEventRouter = express.Router();
+
+const RETRY_INTERVAL = 120000;
+const CLEANUP_INTERVAL = 3600000;
+const CLEANUP_STALE_ENTRIES_THRESHOLD = 43200000;
+          // 12hrs
+
+let connections = {};
+let cleanupTimerID = -1;
+let notifyListenersChangedFlag = false;
+
+let verbose = false;
+
+function log(str) {
+  if (verbose) {
+    console.log('SSE: ' + str);
+  }
+}
+
+///////////////////////////////////////////////////////////////////
+//
+// beg sse funcs (attached to res)
+//
+///////////////////////////////////////////////////////////////////
+function sseHandler(req, res, next) {
+  res.sseSetup = function(close) {
+    let rspCode = close? 404 : 200;  // this isn't working on Chrome
+    let contentType = close? 'text/plaintext' : 'text/event-stream';
+    res.writeHead(rspCode, {
+      'Content-Type': contentType,
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
+
+  res.sseSend = function(retryInterval, data) {
+    let msg = '';
+//    if (retryInterval !== 0)   // chrome is ignoring this...
+//      msg = "retry:" + retryInterval + "\n";
+
+    msg += "data:" + JSON.stringify(data) + "\n\n";
+    res.write(msg);
+  }
+
+  next();
+}
+
+serverEventRouter.use(sseHandler);
+//////////////////////////////////////////////////////////////////////
+//
+// end sse funcs
+// beg async loop funcs
+//
+/////////////////////////////////////////////////////////////////////
+
+function asyncNotifyListenersChanged() {
+  return new Promise(function(acc, rej) {
+    let sseRsps = Object.values(connections);
+    if (sseRsps.length === 0)
+      acc(true);
+
+    else {
+      let msg = "listeners-changed"; 
+                  // work on a copy
+
+      function notifyListenersChanged(ix) {
+        while(true) {
+          if (ix >= sseRsps.length) {
+            acc(true);
+            break;
+          }
+
+          if (sseRsps[ix]['registered-ts'] !== 0) {
+            sseRsps[ix].notifyRes.sseSend(RETRY_INTERVAL, msg);
+            break;
+          }
+          ++ix;
+        }
+
+        setImmediate(notifyListenersChanged.bind(null, ++ix));
+      }
+
+      notifyListenersChanged(0);
+    }
+  });
+}
+
+function asyncCleanupRegistered() {
+  return new Promise(function(acc, rej) {
+    log('in asyncCleanupRegistered');
+    let idKeys = Object.keys(connections);
+                    // pull a copy of the keys
+
+    if (idKeys.length === 0) {
+      log(' --> no connections (no keys)')
+      acc(true); // nothing to do
+    }
+    else {
+      function cleanupRegistered(ix) {
+        if (ix >= idKeys.length)
+          acc(true);
+
+        else {
+          let idKey, sseRsp, duration;
+
+          while (true) {
+                  // loop through the keys
+
+            if (ix >= idKeys.length) {
+              acc(true);
+              break;
+                    // done
+            }
+
+            idKey = idKeys[ix];
+            if (!connections[idKey])
+              continue;
+                    // got deleted in between calls -
+                    // go on to next without exiting
+
+            sseRsp = connections[idKey];
+            duration = Date.now() - sseRsp['registered-ts'];
+            log(' --> duration for id: ' + idKey + ', registered-ts: ' +
+                        sseRsp['registered-ts'] + ' = ' +
+                        duration/1000);
+
+            if (duration > CLEANUP_STALE_ENTRIES_THRESHOLD) {
+              log(' --> removing: ' + idKey + ' from connections');
+              delete connections[idKey];
+                          // thpthpthp - gone.
+              break;
+            }
+                    // else loop around for the next
+            ++ix;
+          }
+          
+          setImmediate(cleanupRegistered.bind(null, ++ix));
+                    // trigger the next iteration
+        }
+      }
+
+      cleanupRegistered(0);
+                    // initial call
+    }
+  });
+}
+
+function startCleanupInterval() {
+  function doCleanup() {
+    cleanupTimerID = setTimeout(function() {
+      clearTimeout(cleanupTimerID);
+      cleanupTimerID = 0;
+      asyncCleanupRegistered()
+      .then(function(rsp){
+        if (Object.keys(connections).length === 0)
+          stopCleanupInterval();
+        else {
+          if (notifyListenersChangedFlag)
+            setTimeout(asyncNotifyListenersChanged);
+          if (cleanupTimerID === 0)
+            setImmediate(doCleanup);
+        }
+      })
+    }, CLEANUP_INTERVAL);
+  }
+
+  if (cleanupTimerID === -1)
+    doCleanup();
+}
+
+function stopCleanupInterval() {
+  if (cleanupTimerID > 0) {
+    clearTimeout(cleanupTimerID);
+    cleanupTimerID = -1;
+  }
+}
+//////////////////////////////////////////////////////////////////
+//
+// end async loop funcs
+// beg ServerEvent object
+//
+///////////////////////////////////////////////////////////////////
+
+let ServerEvent = {
+  unregisterAllListeners() {
+    stopCleanupInterval();
+    connections = {};
+  },
+  setNotifyListenersChanged(flag) {
+    notifyListenersChangedFlag = flag;
+  },
+  setVerbose(flag) {
+    verbose = flag;
+  },
+  notifyListenersChanged() {
+    asyncNotifyListenersChanged();
+  },
+  getUniqueID(name) {
+    let id = '';
+  
+    log('server in make-id: ' + name);
+    
+    do {
+      id = [name, Math.random().toString(36).substring(7)].join('_');
+    } while (id in connections);
+  },
+  registerListener(id, res) {
+    resObj = id in connections? connections[id] : {},
+    timeStamp = Date.now();
+
+    log('in register-listener, id: ' + id);
+
+    if (!(id in connections)) {
+      startCleanupInterval();
+      resObj['notifyRes'] = res;
+      log(' --> server registered id: ' + id);
+    } else {
+      log(' --> is in connections');
+    }
+
+    res.sseSetup();
+    res.sseSend(RETRY_INTERVAL, "registered^" + id);
+    resObj['registered-ts'] = timeStamp;
+    connections[id] = resObj;
+  },
+  isRegistered(id) {
+    return(id in connections);
+  },
+  unregisterListener(id) {
+    if (id in connections)
+      delete connections[id];
+    if (Object.keys(connections).length === 0)
+      stopCleanupInterval();
+  },
+  getListenersJSON() {
+    let idKeys = Object.keys(connections);
+    let rspData = {}, tmpObj;
+    
+    for (let ix = 0; ix < idKeys.length; ++ix) {
+      let idKey = idKeys[ix];
+
+      if (connections[idKey] && connections[idKey]['registered-ts'] !== 0) {
+        tmpObj = Object.assign({}, connections[idKey]);
+        delete tmpObj.notifyRes;
+        rspData[idKey] = tmpObj;
+      }
+    }
+
+    return JSON.stringify(rspData);
+  },
+  notifyCompletions(id, taskid) {
+    let name = id.split('_')[0];
+    return new Promise(function(acc, rej) {
+      let idKeys = Object.keys(connections);
+                // get a current snapshot of the keys
+  
+      if (idKeys.length === 0)
+        acc(true);
+  
+      else {
+        function _notifyCompletions(ix) {
+          if (ix >= idKeys.length)
+            acc(true);
+  
+          else {
+            let idKey = idKeys[ix];
+            if ((idKey in connections) && idKey.startsWith(name)) {
+              connections[idKey].notifyRes.sseSend(RETRY_INTERVAL, "completed^" +
+                JSON.stringify({"timestamp": Date.now(), "id":idKey,
+                                "taskid":taskid}));
+                      // notify target listeners for specific event
+            }
+            
+            setImmediate(_notifyCompletions.bind(null, ++ix));
+          }
+        }
+        
+        _notifyCompletions(0);
+      }
+    })
+  }
+};
+
+module.exports = {ServerEvent, serverEventRouter};
--- app/index.js
+++ app/index.js
@@ -1,300 +1,65 @@
 const express = require('express');
-const sse = require('./sse');
-
+const {ServerEventMgr,serverEventRouter} = require('ServerEvent');
 const app = express();
 
-const RETRY_INTERVAL = 120000;
-const CLEANUP_INTERVAL = 3600000;
-const CLEANUP_STALE_ENTRIES_THRESHOLD = 43200000;
-          // 12hrs
-
-let connections = {};
-let cleanupTimerID = null;
-
-app.use(sse);
-
-function sendResponse(res, msg, code) {
-  if (code === undefined) {
-    code = 200;
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Content-Length, X-Requested-With');
+  // intercept OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+  } else {
+    next();
   }
-  res.writeHead(code, {
-    'Content-Type': 'text/plain',
-    'Cache-Control': 'no-cache',
-    'Access-Control-Allow-Origin': '*',
-    'Content-Length': msg.length
-  });
+};
 
-  res.end(msg);
-}
+app.use(allowCrossDomain);
+app.use(serverEventRouter);
 
-function asyncNotifyListenersChanged() {   
-  return new Promise(function(acc, rej) {
-    let sseRsps = Object.values(connections);
-    if (sseRsps.length === 0)
-      acc(true);
-
-    else {
-      let msg = "listeners-changed"; 
-                  // work on a copy
-
-      function notifyListenersChanged(ix) {
-        while(true) {
-          if (ix >= sseRsps.length) {
-            acc(true);
-            break;
-          }
-
-          if (sseRsps[ix]['registered-ts'] !== 0) {
-            sseRsps[ix].notifyRes.sseSend(RETRY_INTERVAL, msg);
-            break;
-          }
-          ++ix;
-        }
-
-        setImmediate(notifyListenersChanged.bind(null, ++ix));
-      }
-
-      notifyListenersChanged(0);
-    }
-  });
-}
-
-function asyncCleanupRegistered() {
-  return new Promise(function(acc, rej) {
-    console.log('in asyncCleanupRegistered');
-    let idKeys = Object.keys(connections);
-                    // pull a copy of the keys
-
-    if (idKeys.length === 0) {
-      console.log(' --> no connections (no keys)')
-      acc(true); // nothing to do
-    }
-    else {
-      function cleanupRegistered(ix) {
-        if (ix >= idKeys.length)
-          acc(true);
-
-        else {
-          let idKey, sseRsp, duration;
-
-          while (true) {
-                  // loop through the keys
-
-            if (ix >= idKeys.length) {
-              acc(true);
-              break;
-                    // done
-            }
-
-            idKey = idKeys[ix];
-            if (!connections[idKey])
-              continue;
-                    // got deleted in between calls -
-                    // go on to next without exiting
-
-            sseRsp = connections[idKey];
-            duration = Date.now() - sseRsp['registered-ts'];
-            console.log(' --> duration for id: ' + idKey + ', registered-ts: ' +
-                        sseRsp['registered-ts'] + ' = ' +
-                        duration/1000);
-
-            if (duration > CLEANUP_STALE_ENTRIES_THRESHOLD) {
-              console.log(' --> removing: ' + idKey + ' from connections');
-              delete connections[idKey];
-                          // thpthpthp - gone.
-              break;
-            }
-                    // else loop around for the next
-            ++ix;
-          }
-          
-          setImmediate(cleanupRegistered.bind(null, ++ix));
-                    // trigger the next iteration
-        }
-      }
-
-      cleanupRegistered(0);
-                    // initial call
-    }
-  });
-}
-
-function asyncNotifyCompletions(name) {
-  return new Promise(function(acc, rej) {
-    let idKeys = Object.keys(connections);
-              // get a current snapshot of the keys
-
-    if (idKeys.length === 0)
-      acc(true);
-
-    else {
-      function notifyCompletions(ix) {
-        if (ix >= idKeys.length)
-          acc(true);
-
-        else {
-          let idKey = idKeys[ix];
-          if ((idKey in connections) && idKey.startsWith(name)) {
-            connections[idKey].notifyRes.sseSend(RETRY_INTERVAL, "completed^" +
-              JSON.stringify({"timestamp": Date.now(), "id":idKey}));
-                    // notify target listeners for specific event
-          }
-          
-          setImmediate(notifyCompletions.bind(null, ++ix));
-        }
-      }
-      
-      notifyCompletions(0);
-    }
-  })
-}
+ServerEventMgr.setNotifyListenersChanged(true);
 
 app.get('/make-id/:name', function(req,rsp) {
-  let name = req.params.name;
-  let id = '';
-
-  console.log('server in make-id: ' + name);
-  
-  do {
-    id = [name, Math.random().toString(36).substring(7)].join('_');
-  } while (id in connections);
-
-  sendResponse(rsp, id);
+  let id = ServerEventMgr.getUniqueID(req.params.name);
+  rsp.send(id);
 });
 
-app.get('/submitted/:id', function(req, res) {
-  let id = req.params.id;
-  console.log('server in submitted: ' + id);
-  if (id in connections) {
-    connections[id].singleShot = req.params.singleShot || false;
-
-    setTimeout(function(){
-      asyncNotifyListenersChanged()
-                // notify everyone of listener changes
-      .then(function(rsp){
-        setTimeout(function() {
-          let name = id.split('_')[0];
-          asyncNotifyCompletions(name);
-        }, 5000);
-      });
-    });
-
-    sendResponse(res, "Submitted for: " + id);
-  } else {
-    sendResponse(res, "Not registered", 404);
+app.put('/submit-task/:id/:taskid', function(req, res) {
+  let id = req.params.id, taskid = req.params.taskid;
+  if (ServerEventMgr.isRegistered(id)) {
+    setTimeout(async ()=>{ await ServerEventMgr.notifyCompletions(id, taskid)}, 5000);
+    res.send("ok")
   }
-});
-
-app.get('/trigger-server-response/:id', function(req,res) {
-  let id = req.params.id;
-  console.log('in trigger-server-response: ' +  id);
-
-  if (id in connections) {
-    connections[id].notifyRes.sseSend(RETRY_INTERVAL, "triggered^" +
-      JSON.stringify({"timestamp": Date.now(), "id":id}));
-  }
-  else {
-    console.log(" --> not in connections - no trigger");
-  }
-
-  sendResponse(res,'ok');
-});
-
-app.get('/trigger-cleanup', function(req,res) {
-  setTimeout(asyncCleanupRegistered);
-  sendResponse(res, 'ok');
+  else
+    res.status(404).send("not found");
 });
 
 app.get('/list-registrants', function(req, res) {
-  let idKeys = Object.keys(connections);
-  let rspData = {}, tmpObj;
-  
-  for (let ix = 0; ix < idKeys.length; ++ix) {
-    let idKey = idKeys[ix];
-
-    if (connections[idKey] && connections[idKey]['registered-ts'] !== 0) {
-      tmpObj = Object.assign({}, connections[idKey]);
-      delete tmpObj.notifyRes;
-      rspData[idKey] = tmpObj;
-    }
-  }
-
-  sendResponse(res, JSON.stringify(rspData));
+  res.send(ServerEventMgr.getListenersJSON());
 });
 
 app.get('/clear-registrants', function(req, res) {
-  stopCleanupInterval();
-  connections = {};
-  sendResponse(res, "cleared");
-  asyncNotifyListenersChanged();
-  startCleanupInterval();
+  await ServerEventMgr.unregisterAllListeners();
+  res.send("ok");
 });
 
 /**
  * register a listener
  */
 app.get('/register-listener/:id', function(req, res) {
-  let id = req.params.id,
-      resObj = id in connections? connections[id] : {},
-      timeStamp = Date.now();
-
-  console.log('in register-listener, id: ' + id);
-
-  if (!(id in connections)) {
-    resObj['notifyRes'] = res;
-    console.log(' --> server registered id: ' + id);
-  } else {
-    console.log(' --> is in connections');
-  }
-
-  res.sseSetup();
-  res.sseSend(RETRY_INTERVAL, "registered^" + id);
-  resObj['registered-ts'] = timeStamp;
-  connections[id] = resObj;
+  let id = req.params.id;
+  ServerEventMgr.registerListener(id, res);
 });
 
 app.get('/disconnect-registrant/:id', function(req,res){
-  let id = req.params.id;
-  console.log('in disconnect-registrant, id: ' + id);
-  if (id in connections) {
-    delete connections[id];
-    console.log(' --> deleted: ' + id);
-    asyncNotifyListenersChanged();
-  }
-
-  sendResponse(res, 'ok');
+  ServerEventMgr.unregisterListener(req.params.id);
+  res.send('ok');
 });
 
 app.get('/testme', function(req, res) {
   console.log('server: in testme');
   sendResponse(res, "I'm here!!!\n");
 });
-
-function stopCleanupInterval() {
-  if (cleanupTimerID > 0) {
-    clearTimeout(cleanupTimerID);
-    cleanupTimerID = -1;
-  }
-}
-
-function startCleanupInterval() {
-  function doCleanup() {
-    cleanupTimerID = setTimeout(function() {
-      clearTimeout(cleanupTimerID);
-      cleanupTimerID = 0;
-      asyncCleanupRegistered()
-      .then(function(rsp){
-        setTimeout(asyncNotifyListenersChanged);
-        if (cleanupTimerID === 0)
-          setImmediate(doCleanup);
-      })
-    }, CLEANUP_INTERVAL);
-  }
-
-  doCleanup();
-}
-
-startCleanupInterval();
 
 app.listen(3000);
 console.log('Listening on port 3000');
